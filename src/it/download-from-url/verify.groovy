c7590fb0a975781792b39e419c5e1194b22d9d6d
Kyle Lieber <klieberuni@gmail.com>
2014-05-02 20:34:42 -0500

    updating integration tests

--- src/it/download-from-url/verify.groovy
+++ src/it/download-from-url/verify.groovy
@@ -18,10 +18,29 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-import org.codehaus.plexus.util.*;
+import org.codehaus.plexus.util.FileUtils
+
+def platform = System.properties['os.name'].toLowerCase();
+
+def expected = 'target/phantomjs-maven-plugin/phantomjs-1.9.6-';
+
+if (platform.contains('win')) {
+    expected += 'windows/phantomjs.exe';
+} else if (platform.contains('mac')) {
+    expected += 'macosx/bin/phantomjs';
+} else if (platform.contains('nux')) {
+    def arch = System.properties['os.arch'].contains('64') ? 'x86_64' : 'i686';
+    expected += 'linux-' + arch + '/bin/phantomjs';
+}
+
+File binary = new File(basedir, expected);
+
+assert binary.isFile()
+assert binary.canExecute()
 
 String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'));
 
+assert buildLog.contains('Downloading phantomjs binary') : 'phantomjs binaries were not downloaded.';
 assert !buildLog.contains('phantomjs.binary.path=null') && !buildLog.contains('"phantomjs.binary.path":"null"') : "phantomjs.binary property was not properly set"
-assert buildLog.contains('Tests run: 1, Failures: 0, Errors: 0, Skipped: 0') : 'junit tests failed to execute'
-assert buildLog.contains('BUILD SUCCESS') : 'build was not successful'
+assert buildLog.contains('Hello, world!'): 'phantomjs script execution failed.';
+
--- src/it/with-jasmine-maven-plugin-it/verify.groovy
+++ src/it/with-jasmine-maven-plugin-it/verify.groovy
@@ -2,7 +2,6 @@ import org.codehaus.plexus.util.*;
 
 String buildLog = FileUtils.fileRead(new File(basedir, 'build.log'));
 
-assert buildLog.contains('Downloading phantomjs binary') : 'phantomjs binaries were not downloaded.';
 assert !buildLog.contains('phantomjs.binary.path=null') && !buildLog.contains('"phantomjs.binary.path":"null"') : "phantomjs.binary property was not properly set"
 assert buildLog.contains('Results: 1 specs, 0 failures') : 'jasmine specs failed to execute'
 assert buildLog.contains('BUILD SUCCESS') : 'build was not successful'
